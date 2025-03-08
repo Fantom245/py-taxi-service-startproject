e4ff6da616ba25e0df7cf7fbb4d2c17e46fde63f
unknown <gyrkalomas@gmail.com>
2025-03-09 01:45:55 +0200

    Solution

---
+++ taxi/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+
+from taxi.models import Manufacturer, Car, Driver
+
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ["model",]
+
+    list_filter = ["manufacturer",]
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = ["username", "email", "first_name", "last_name", "license_number"]
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
+
+
+admin.site.register(Manufacturer, ManufacturerAdmin)
